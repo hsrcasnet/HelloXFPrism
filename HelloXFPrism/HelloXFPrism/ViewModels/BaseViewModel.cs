b6e9af40e7a0e841783982014ade398dfbf5efde
Thomas Galliker <thomas@bluewin.ch>
2019-03-23 11:21:31 +0100

    Replace INotifyPropertyChanged implementation with BindableBase (Prism)

--- HelloXFPrism/HelloXFPrism/ViewModels/BaseViewModel.cs
+++ HelloXFPrism/HelloXFPrism/ViewModels/BaseViewModel.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
-namespace HelloXFPrism.ViewModels
+﻿namespace HelloXFPrism.ViewModels
 {
-    public class BaseViewModel : INotifyPropertyChanged
+    public class BaseViewModel : Prism.Mvvm.BindableBase
     {
         private bool isBusy = false;
         private string title = string.Empty;
@@ -21,37 +16,5 @@ namespace HelloXFPrism.ViewModels
             get => this.title;
             set => this.SetProperty(ref this.title, value);
         }
-
-        protected bool SetProperty<T>(ref T backingStore, T value,
-            [CallerMemberName] string propertyName = "",
-            Action onChanged = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(backingStore, value))
-            {
-                return false;
-            }
-
-            backingStore = value;
-            onChanged?.Invoke();
-            this.OnPropertyChanged(propertyName);
-            return true;
-        }
-
-        #region INotifyPropertyChanged
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            var changed = this.PropertyChanged;
-            if (changed == null)
-            {
-                return;
-            }
-
-            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        #endregion
     }
 }
